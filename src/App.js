a401a84765fcbeda8a0636ea26d2951e7dc1c9ec
Alisher <info@regname.uz>
2022-04-25 00:51:24 +0500

    start redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import {useSelector, useDispatch} from 'react-redux';
+
+import { incCountAction, decCountAction } from './store/countReducer';
+
+
 function App() {
+  const dispatch = useDispatch();
+  const count = useSelector(state => state.count.count);
+  const customers = useSelector(state => state.customers.customers);
+ 
+  const addCustomer = (name) => {
+    if(!name) return
+    const customer = {
+      name,
+      id: Date.now(),
+    }
+    dispatch({type:"ADD_CUSTOMER", payload:customer})
+  }
+  const removeCustomer = (customer) => {
+    dispatch({type:"REMOVE_CUSTOMER", payload:customer.id})
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>{count}</p>
+        <button onClick={() => {dispatch(incCountAction())}}>INC</button>
+        <button  onClick={() => {dispatch(decCountAction())}}>DEC</button>
+        <button onClick={() => {addCustomer(prompt())}}>Добавить пользователей</button>
+        
+        <ul>
+          {customers.length>0 ?
+            customers.map(customer => {
+              return (
+                <li onClick={() => {removeCustomer(customer)}} key={customer.id}>{customer.name}</li>
+              )
+            })
+            :
+            <span>пусто</span>
+          }
+        </ul>
     </div>
   );
 }
